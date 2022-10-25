654b08a0a81ca970d540250f69fe16b78f3b677b
Vlodkow <vlodkow@gmail.com>
2022-10-25 22:26:46 +0300

    Update template

--- frontend/src/components/MyMessages/OneMessage.js
+++ frontend/src/components/MyMessages/OneMessage.js
@@ -58,7 +58,7 @@ export const OneMessage = ({ message, opponent, isLast, setReplyToMessage }) => 
             </div>
           )}
 
-          <div className={`flex items-center group ${message.isMy ? "" : "flex-row-reverse justify-end"}`}>
+          <div className={`flex items-center group relative ${message.isMy ? "" : "flex-row-reverse justify-end"}`}>
             {message.isTemporary && (
               <div className={"mr-2 text-gray-400 opacity-60"}>
                 <BsClockHistory size={16}/>
@@ -82,14 +82,13 @@ export const OneMessage = ({ message, opponent, isLast, setReplyToMessage }) => 
             )}
 
             <div className={`max-w-[260px] md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl whitespace-pre-wrap px-5 
-              overflow-hidden overflow-ellipsis text-base relative min-h-[46px]
+              overflow-ellipsis text-base relative min-h-[46px]
               ${message.text === '(like)' ? "py-2.5" : "py-3"}
               ${message.isFirst && message.isMy ? "rounded-t-3xl" : ""}
               ${isLast ? "rounded-b-3xl" : ""}
               ${message.isTemporary ? "opacity-70" : ""}
               ${message.isMy ? "bg-sky-500/50 rounded-l-3xl ml-2" : "bg-gray-700/60 rounded-r-3xl text-gray-100 mr-2"}
               ${message.isEncryptStart || message.isEncryptAccept || message.isEncryptEnd ? "bg-red-600/30" : ""}
-              ${message.encrypt_key && (message.isMy ? "pl-12" : "pr-12")}
             `}>
 
               {message.reply_message && (
@@ -139,9 +138,10 @@ export const OneMessage = ({ message, opponent, isLast, setReplyToMessage }) => 
               {message.encrypt_key && (
                 <img src={require("../../assets/img/lock.png")}
                      alt=""
-                     className={`absolute opacity-20 mx-2 w-5 h-5 top-3.5 ${message.isMy ? "left-2" : "right-2"}`}/>
+                     className={`absolute w-4 h-4 top-0 ${message.isMy ? "-left-1" : "-right-1"}`}/>
               )}
             </div>
+
           </div>
         </div>
       </div>
