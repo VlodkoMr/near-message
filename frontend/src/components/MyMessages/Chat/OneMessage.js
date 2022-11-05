a05e68f9d4a407e9be9e4274e9d3be308a69f15a
Vlodkow <vlodkow@gmail.com>
2022-11-05 20:29:12 +0200

    Update styles

--- frontend/src/components/MyMessages/Chat/OneMessage.js
+++ frontend/src/components/MyMessages/Chat/OneMessage.js
@@ -105,7 +105,8 @@ export const OneMessage = ({ message, opponent, isLast, setReplyToMessage }) => 
               )}
 
               {message.deposit > 0 && (
-                <div className={`text-sm bg-lime-600 whitespace-nowrap rounded-lg py-1 px-3 font-semibold mt-1 mb-2`}>
+                <div className={`text-sm whitespace-nowrap rounded-lg py-2 px-3 font-semibold mt-1 mb-2
+                ${message.isMy ? "bg-sky-600" : "bg-red-800"}`}>
                   Deposit: {utils.format.formatNearAmount(message.deposit)} NEAR
                 </div>
               )}
