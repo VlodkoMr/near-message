959317fb650046649c02133d997bac80ceb408e4
vlodkow <vlodkow@gmail.com>
2022-12-22 19:42:15 +0200

    FAQ text updates

--- frontend/src/pages/Faq.js
+++ frontend/src/pages/Faq.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
 import { Layout } from "./Layout";
 
 export const Faq = () => {
@@ -8,22 +7,24 @@ export const Faq = () => {
   const OneQuestion = ({ title, index, children }) => (
     <div
       onClick={() => setOpenedIndex(index)}
-      className={`mb-6 relative cursor-pointer ${index !== 16 ? "border-b border-white/20" : "mb-20"}`}>
+      className={`mb-6 relative 
+      ${index === openedIndex ? "cursor-default" : "cursor-pointer"}
+      ${index !== 16 ? "border-b border-white/20" : "mb-20"}`}>
       <div
         className={`absolute w-10 h-10 rounded-full text-center middle top-0 right-0
         ${
           index === openedIndex
-            ? "bg-sky-500 cursor-default opacity-70"
+            ? "bg-sky-900 cursor-default opacity-50"
             : "bg-sky-900 cursor-pointer"
         }`}
       >
         <span className="inline-block pt-1 text-2xl font-semibold">
-          {index === openedIndex ? "-" : "+"}
+          {index === openedIndex ? "−" : "+"}
         </span>
       </div>
 
       <div className={`text-2xl font-semibold mb-6`}>{title}</div>
-      <div className={`text-gray-200 ${index === openedIndex ? "h-auto mb-6" : "h-0"} pr-16 overflow-hidden transition`}>
+      <div className={`text-gray-300/90 ${index === openedIndex ? "h-auto mb-6" : "h-0"} pr-16 overflow-hidden transition`}>
         {children}
       </div>
     </div>
@@ -43,10 +44,16 @@ export const Faq = () => {
             index={1}
           >
             <p>
-              ChatMe is chats and messages service build on NEAR blockchain that combine the best user experience, security, fast and cheap
-              transactions with new web3 possibilities. Our service provide direct messages between native NEAR accounts, public and private
-              chats, broadcasting channels and other great features for communication and data sharing. ChatMe is open-source and well
-              documented to simplify our features integration into any project in NEAR ecosystem.
+              ChatMe is chats and messages service build on
+              <a href="https://near.org/" className={"faq-link"} target={"_blank"}>
+                NEAR blockchain
+              </a>
+              that combine the best user experience, security, fast and cheap transactions with new web3 possibilities.
+            </p>
+            <p className={"mt-3"}>
+              Our service provide direct messages between native NEAR accounts, public and private chats, broadcasting channels and other
+              great features for communication and data sharing. ChatMe is open-source and well documented to simplify integration into
+              any project in NEAR ecosystem.
             </p>
           </OneQuestion>
 
@@ -58,7 +65,7 @@ export const Faq = () => {
               To use ChatMe you need only NEAR account with small amount of NEAR tokens. If you don't have this account, we prepared more
               details below: how to create new account and get NEAR tokens.
             </p>
-            <p>
+            <p className={"mt-3"}>
               When account created, just click "Connect Wallet" button in top-left corner of our website and select wallet that you used to
               create you account. On next step allow ChatMe to get access to your account (access only for 0.25 NEAR to cover blockchain
               transactions). When your account connected click "Launch App", you will be able to send messages, create groups and use our
@@ -72,22 +79,37 @@ export const Faq = () => {
           >
             <p>
               <b>Option 1 - desktop browser or ledger hardware wallet.</b><br/>
-              It is really simple process, just open: https://wallet.near.org and click "Create Account". Follow few steps guide to receive
+              It is really simple process, just open:
+              <a href="https://wallet.near.org" className={"faq-link"} target={"_blank"}>
+                https://wallet.near.org
+              </a>
+              and click "Create Account". Follow few steps guide to receive
               Seed Phrase (save it in secure place and NEVER share with other users, this phrase provide full access to your wallet). As
               result you will have registered wallet address (unique 64 characters string) that can be used to deposit NEAR tokens and use
               ChatMe service.
             </p>
             <p className={"mt-3"}>
               <b>Option 2 - mobile phone.</b><br/>
-              You can install HERE wallet in your iOS or Android phone. Follow simple instructions to create new account in your mobile
-              phone and deposit NEAR tokens. In chatme website click "Connect Wallet", chose HERE wallet and scan QR-code to allow access to
-              your wallet for our service.
+              You can install
+              <a href="https://herewallet.app/" className={"faq-link"} target={"_blank"}>
+                HERE wallet
+              </a>
+              in your iOS/Android phone. Follow simple instructions to create new account in your mobile
+              phone and deposit NEAR tokens. In ChatMe website click "Connect Wallet", chose HERE wallet and scan QR-code to allow access to
+              your account for our service.
             </p>
             <p className={"mt-3"}>
               <b>Option 3 - browser wallet extensions.</b><br/>
-              You can install Meteor wallet extension for Google Chrome or Brave browser and use it to connect with our service. You can
-              find all details how to create new account using Meteor wallet in their website "Simple Onboarding" section. Then deposit NEAR
-              tokens and you will be able to use it with our service.
+              Install
+              <a href="https://meteorwallet.app/" className={"faq-link"} target={"_blank"}>
+                Meteor wallet
+              </a>
+              extension for Google Chrome or Brave browser and use it to connect with our service. You can
+              find all details how to create new account using Meteor wallet in their website
+              <a href="https://meteorwallet.app/" className={"faq-link"} target={"_blank"}>
+                "Simple Onboarding"
+              </a>
+              section. Next step is deposit NEAR tokens to use it with our service.
             </p>
           </OneQuestion>
 
@@ -97,21 +119,30 @@ export const Faq = () => {
           >
             <ul>
               <li>
-                Register account in crypto exchange or use existing one. You can use one of the exchanges: Binance, Huobi, Kukoin.
+                1. Register account in crypto exchange or use existing one. You can use one of the exchanges:
+                <a href="https://www.binance.com/" className={"faq-link"} target={"_blank"}>
+                  Binance
+                </a>,
+                <a href="https://www.huobi.com/" className={"faq-link"} target={"_blank"}>
+                  Huobi
+                </a>,
+                <a href="https://www.kucoin.com/" className={"faq-link"} target={"_blank"}>
+                  Kukoin.
+                </a>
               </li>
               <li>
-                After registration you may need to verify/approve your account.
+                2. After registration you may need to verify/approve your account.
               </li>
               <li>
-                Now you can deposit fiat currency using one of provided payment methods that best suits for you.
+                3. Now you can deposit fiat currency using one of provided payment methods that best suits for you.
               </li>
               <li>
-                After deposit confirmation you can exchange your funds for NEAR. On easier-to-use exchanges, this is as easy as entering the
-                amount you want to purchase and clicking buy.
+                4. After deposit confirmation you can exchange your funds for NEAR. On easier-to-use exchanges, this is as easy as entering
+                the amount you want to purchase and clicking buy.
               </li>
               <li>
-                Withdraw NEAR tokens to your Wallet Address (existing or registered on previous step). Use "NEAR" network when you withdraw
-                your tokens.
+                5. Withdraw NEAR tokens to your Wallet address (existing or registered on previous step). Use "NEAR" network when you
+                withdraw your tokens.
               </li>
             </ul>
           </OneQuestion>
@@ -137,15 +168,6 @@ export const Faq = () => {
           </OneQuestion>
 
           <OneQuestion
-            title={"Can I start using ChatMe for free?"}
-            index={7}
-          >
-            <p>
-              You can start for free, all you need is small amount of NEAR tokens to cover blockchain gas usage.
-            </p>
-          </OneQuestion>
-
-          <OneQuestion
             title={"Why we choose NEAR blockchain"}
             index={8}
           >
@@ -156,37 +178,44 @@ export const Faq = () => {
           </OneQuestion>
 
           <OneQuestion
+            title={"Can I start using ChatMe for free?"}
+            index={7}
+          >
+            <p>
+              Yes, you can start for free, all you need is small amount of NEAR tokens to cover blockchain gas usage.
+            </p>
+          </OneQuestion>
+
+          <OneQuestion
             title={"Difference between ChatMe and other chatting apps"}
             index={9}
           >
             <p>
               For telegram or other messenger you connect by phone number or nickname. ChatMe can be used directly with native NEAR address
-              and we build it to cover NEAR accounts and ecosystem needs. We don't try to replace telegram, viber, whatsApp or other
+              and we build it to cover NEAR ecosystem needs. We don't try to replace telegram, viber, whatsApp or other
               messenger, but who knows :)
             </p>
           </OneQuestion>
 
           <OneQuestion
-            title={"Please provide more details about encoding and private mode"}
+            title={"Please provide more details about encoding and Private Mode"}
             index={10}
           >
             <p>
               All data in blockchain is publicly available but for direct messages we build private mode that encode data before sending to
-              blockchain and decode in other side. For this encoding and decoding process we use near-js-encryption-box library. In any case
-              we recommend to not send passwords or other important information because maybe in future current encoding algorithms can
-              become insecure. You can find more details how encryption works and security details in our documentation.
+              blockchain and decode in other side. For this process we use
+              <a href="https://github.com/NEARFoundation/near-js-encryption-box" className={"faq-link"} target={"_blank"}>
+                near-js-encryption-box library
+              </a>.
             </p>
-          </OneQuestion>
-
-          <OneQuestion
-            title={"Our team history and experience"}
-            index={11}
-          >
             <p>
-              We are team of talented people that work with web3 projects in different chains but NEAR is our favourite one. We also create
-              ZomLand NFT collectible game on NEAR. In general chatMe was build in 3 months by Vlodkow and if we talk about my experience: I
-              started from web development 15 years ago and completely switched to web3 development 2 years ago. Main our focus at the
-              moment is to build quality applications and games to extend NEAR ecosystem.
+              To start using private mode your account need Gold Level. Open direct conversation and click "lock" icon to request privacy
+              mode. You will receive a notification when request accepted and Private Mode will automatically be turned on
+              (red color for the background and icons show that Private Mode is enabled).
+            </p>
+            <p className={"mt-3 text-red-300"}>
+              We recommend to not send passwords or other important information because maybe in future current encoding algorithms can
+              become insecure. You can find more details how encryption works and security details in our documentation.
             </p>
           </OneQuestion>
 
@@ -209,7 +238,7 @@ export const Faq = () => {
             <p>
               Main role and value - we provide communication for accounts and projects in NEAR ecosystem. Any project can implement our
               features and receive all benefits of communication between accounts or groups of people. For users it extend their abilities
-              and provide central point for all conversations and chats: if you use different services that integrated with chatMe, have
+              and provide central point for all conversations and chats: if you use different services that integrated with ChatMe, have
               direct messages with accounts or participate in groups - all this chats and channels will be available in our main website.
             </p>
           </OneQuestion>
@@ -225,15 +254,38 @@ export const Faq = () => {
           </OneQuestion>
 
           <OneQuestion
-            title={"Do we plan to collaborate with other projects in the Near ecosystem?"}
+            title={"Do we collaborate with other projects in the NEAR ecosystem?"}
             index={15}
           >
             <p>
-              When we collaborate with project it mean that our projects implement some features. For example chatme support work with
-              wallets that you describe. Another example is chatMe integration in ZomLand that automatically create private chats for
-              communication inside clans. It mean that when ZomLand player create new clan, we call chatMe to create private group for this
-              clan. If another player join or leave this clan, chat participants will be also updated. Each clan have separate page where
-              participants can communicate to build their own strategies to win clan battles or reach other goals.
+              Yes, we opened and ready for technical and social collaborations with other teams and projects.
+              You can find ChatMe use cases in our
+              <a href="https://chatme.gitbook.io/chatme/use-cases-and-benefits" className={"faq-link"} target={"_blank"}>documentation</a>
+              and integrate our features in your project (technical partnership). For social partnerships please contact us to discuss
+              details.
+            </p>
+          </OneQuestion>
+
+          <OneQuestion
+            title={"Our team history and experience"}
+            index={11}
+          >
+            <p>
+              We are team of talented people that work with web3 projects in different chains but NEAR is our favourite one. We also create
+              <a href="https://zomland.com/" className={"faq-link"} target={"_blank"}>
+                ZomLand
+              </a>
+              NFT collectible game on NEAR. Main our focus at the moment is to build quality applications and games to extend NEAR
+              ecosystem.
+            </p>
+            <p className={"mt-3"}>
+              In general ChatMe was build in 3 months by
+              <a href="https://twitter.com/vlodkowZ" className={"faq-link"} target={"_blank"}>
+                Vlodkow
+              </a>
+              and if we talk about my experience:
+              I started from web development 15 years ago and completely switched to web3 development 2 years ago to find new
+              possibilities and move development to new level.
             </p>
           </OneQuestion>
 
@@ -242,8 +294,35 @@ export const Faq = () => {
             index={16}
           >
             <p>
-              Our documentation contain all information about chatMe service and join our social channels to get updates.
-              If you have additional questions, you can ask in discord:
+              Our
+              <a href="https://chatme.gitbook.io/chatme/" className={"faq-link"} target={"_blank"}>
+                documentation
+              </a>
+              contain all information about ChatMe. <br/>
+              Join our social channels to get updates:
+              <ul className={"mt-3"}>
+                <li>
+                  <a href="https://twitter.com/chatme_near" className={"faq-link"} target={"_blank"}>
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a href="https://t.me/chatme_near" className={"faq-link"} target={"_blank"}>
+                    Telegram
+                  </a>
+                </li>
+                <li>
+                  <a href="/my/group/1" className={"faq-link"}>
+                    ChatMe public Channel
+                  </a>
+                </li>
+              </ul>
+            </p>
+            <p className={"mt-3"}>
+              If you have additional questions, you can ask in our
+              <a href="https://discord.gg/pcvvn4EJpa" className={"faq-link"} target={"_blank"}>
+                discord.
+              </a>
             </p>
           </OneQuestion>
 
